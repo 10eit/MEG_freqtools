785ec7b79c7df52d26d3b1e9efc64f6cccd98e1b
Yanlin Fu <deceit128@gmail.com>
2025-03-20 21:57:34 +0800

    add auto cycle calculation for power estimation

---
+++ meg_freqtools/utils/power_estimate.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+
+def auto_cycle(freq_bins,duration):
+    """
+    freq_bins : list or numpy array
+    duration : means epoch time in second
+    """
+    cpoint = np.where(freq_bins < 8)[0][-1] ## find the last frequency that is smaller than 8 Hz
+    cycle_slow_freq = duration * freq_bins[:cpoint]
+
+    if cycle_slow_freq.min() 
+
+    cycle_array = np.zeros_like(freq_bins)
+    cycle_array[cpoint + 1:] = freq_bins[cpoint + 1:] // 2
